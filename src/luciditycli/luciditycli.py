9c60f2def5bcdd14d0e6ae77017371f92278590e
qwikster <ocoincs@gmail.com>
2025-09-24 23:14:56 -0400

    theme constructor complete

--- src/luciditycli/luciditycli.py
+++ src/luciditycli/luciditycli.py
@@ -1,7 +1,9 @@
 import configparser # TODO: ADD FORMATTERS, DONE? TEST, NEED NEXT FUNCTIONS, VERY GOOD
+import threading
 import time
 
 def test_func():
+    time.sleep(0.1)
     print("hi")
 
 class Theme:
@@ -24,17 +26,21 @@ class Theme:
 
         self.col_reset = "\x1b[0m"
         self.col_clear = "\x1b[2J \x1b[H"
+        
+        self.storage["DEFAULT"]["current_theme"] = name
+        with open(self.path, 'w') as configfile:
+            self.storage.write(configfile)
     
     def get_themes(self) -> list: # get list of theme names
         return self.storage.sections()
     
     def get_colors(self, theme: str) -> list: # returns 2d list [[name, r, g, b], [name, r, g, b]...]
         result = []
-
-        for i in self.storage[theme]:
-            temp = [i]
-            temp.append(self.rgb_str_to_list(self.storage[theme][i]))
-            result.append(temp)
+        for key, value in self.storage[theme].items():
+            if key == "current_theme":
+                continue
+            rgb = self.rgb_str_to_list(value)
+            result.append([key, rgb[0], rgb[1], rgb[2]])
         return result
     
     def rgb_str_to_list(self, str_in: str) -> list[int]:
@@ -52,7 +58,7 @@ class Theme:
         try:
             RGB = f"{list_in[0]};{list_in[1]};{list_in[2]}"
         except Exception:
-            return("Malformed RGB string")
+            raise ValueError # Malformed RGB string
 
         if bg:
             return f"\x1b[48;2;{RGB}m"
@@ -63,17 +69,23 @@ class Theme:
         return self.get_colors(self.current)
     
     def new_theme(self, data: list) -> None:
-        '''
-        creates a new theme and saves it to disk
-        data - 2d list [name, [name, r, g, b], [name, r, g, b]...]
-        '''
-        for item in data:
-            if type(item) is str: # create entry
-                name = item
-                self.storage[name] = {}
-            else:
-                key = item[0]
-                self.storage[name][key] = f"{item[1]}, {item[2]}, {item[3]}"
+        """
+        Creates a new theme and saves it to disk.
+        data: dict of the form
+        {
+            "theme_name": {
+                "title": [r, g, b],
+                "text": [r, g, b],
+                ...
+            }
+        }
+        """
+        for theme_name, colors in data.items():
+            self.storage[theme_name] = {}
+            for key, rgb in colors.items():
+                if len(rgb) != 3:
+                    raise ValueError(f"Invalid RGB for {key}: {rgb!r}")
+                self.storage[theme_name][key] = f"{rgb[0]}, {rgb[1]}, {rgb[2]}"
         
         with open(self.path, 'w') as configfile:
             self.storage.write(configfile)
@@ -102,11 +114,46 @@ class Theme:
         print("╞ ╪ ╡  ╟ ╫ ╢   ┄ ┅    ┆ ┇  ")
         print("╘ ╧ ╛  ╙ ╨ ╜   ┈ ┉    ┊ ┋  ")
 
+class Listener:
+    def __init__(self):
+        listener_thread = threading.Thread(target=self.key_listener, args=(self.handle_key,), daemon=True)
+        listener_thread.start()
+        
+    def handle_key(key: str):
+        if key == "t":
+            active = False
+            theme_menu()
+            active = True
+            
+        elif key == "q":
+            global doquit # if you call sys.exit it'll just quit the listener
+            doquit = True
+            sys.exit(0)
+            
+        elif key == "u":
+            global req_user
+            active = False
+            req_user = get_user()
+            request()
+            if read(api_response.TODAY, "data.username") == f"{color.ERROR}response parser brokey":
+                print(f"{color.ERROR}Invalid user!")
+                print_buffer()
+                time.sleep(3)
+                req_user = "my"
+                request()
+            active = True
+        else:
+            pass # other functions might need to go here?
 
 theme = Theme('example.cfg')
-prev = theme.preview("theme_name")
+prev = theme.preview("solarized")
 for i in prev:
     print(i)
 
-data = ["theme_name", ["background", 64, 128, 255], ["color1", 255, 255, 255], ["test_col", 0, 0, 0]]
-theme.new_theme(data)
+# theme.new_theme({
+#     "solarized": {
+#         "title": [255, 255, 255],
+#         "text": [131, 148, 150],
+#         "error": [220, 50, 47]
+#     }
+# })
